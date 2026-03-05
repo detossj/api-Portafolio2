07ad87bf652981b86e4d9ee1e5d61bb50e6fb506
detossj <jorgerubilarssj@gmail.com>
2026-03-05 13:55:49 -0300

    Se se agregaron las rutas de PortfolioController en api.php

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PortfolioController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('portfolio')->group(function () {
+    Route::get('/nav', [PortfolioController::class, 'getNav']);
+    Route::get('/hero', [PortfolioController::class, 'getHero']);
+    Route::get('/about', [PortfolioController::class, 'getAbout']);
+    Route::get('/skills', [PortfolioController::class, 'getSkills']);
+    Route::get('/education', [PortfolioController::class, 'getEducation']);
+    Route::get('/experience', [PortfolioController::class, 'getExperience']);
+    Route::get('/projects', [PortfolioController::class, 'getProjects']);
+    Route::get('/contact', [PortfolioController::class, 'getContact']);
+    Route::get('/footer', [PortfolioController::class, 'getFooter']);
+});
