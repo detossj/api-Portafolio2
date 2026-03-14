b53c8a1769d828c361dca434552c120f03d7c0f4
detossj <jorgerubilarssj@gmail.com>
2026-03-14 12:13:06 -0300

    Se agregaron los inputs al formulario en GithubRepoForm.php

--- app/Filament/Resources/GithubRepos/Schemas/GithubRepoForm.php
+++ app/Filament/Resources/GithubRepos/Schemas/GithubRepoForm.php
@@ -3,6 +3,9 @@
 namespace App\Filament\Resources\GithubRepos\Schemas;
 
 use Filament\Schemas\Schema;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TagsInput;
 
 class GithubRepoForm
 {
@@ -10,7 +13,11 @@ class GithubRepoForm
     {
         return $schema
             ->components([
-                //
+                TextInput::make('name')->required()->label('Nombre del Repositorio'),
+                TextInput::make('url')->required()->url()->label('URL de GitHub'),
+                TextInput::make('language')->label('Lenguaje Principal (Ej: PHP, Kotlin)'),
+                TextInput::make('stars')->numeric()->label('Estrellas (Opcional)')->default(0),
+                Textarea::make('description')->label('Descripción corta')->columnSpanFull(),
             ]);
     }
 }
