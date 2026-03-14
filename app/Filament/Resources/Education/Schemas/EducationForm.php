f024f1b9078a54e60b0e51f2e2c5a6e85ab417bf
detossj <jorgerubilarssj@gmail.com>
2026-03-14 12:01:06 -0300

    Se agregaron los inputs al formulario en EducationForm.php

--- app/Filament/Resources/Education/Schemas/EducationForm.php
+++ app/Filament/Resources/Education/Schemas/EducationForm.php
@@ -2,6 +2,8 @@
 
 namespace App\Filament\Resources\Education\Schemas;
 
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
 use Filament\Schemas\Schema;
 
 class EducationForm
@@ -10,7 +12,12 @@ class EducationForm
     {
         return $schema
             ->components([
-                //
+                TextInput::make('degree')->required()->label('Título / Carrera'),
+                TextInput::make('institution')->required()->label('Institución'),
+                TextInput::make('status')->required()->label('Estado (Ej: En curso)'),
+                TextInput::make('start_date')->required()->label('Año Inicio'),
+                TextInput::make('end_date')->required()->label('Año Fin'),
+                Textarea::make('description')->required()->label('Descripción')->columnSpanFull(),
             ]);
     }
 }
