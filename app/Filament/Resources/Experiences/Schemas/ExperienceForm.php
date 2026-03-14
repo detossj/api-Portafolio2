73b04cdb6dbb62bc1824e4edd03227eaee501397
detossj <jorgerubilarssj@gmail.com>
2026-03-14 12:06:41 -0300

    Se agregaron los inputs al formulario en ExperienceForm.php

--- app/Filament/Resources/Experiences/Schemas/ExperienceForm.php
+++ app/Filament/Resources/Experiences/Schemas/ExperienceForm.php
@@ -3,6 +3,9 @@
 namespace App\Filament\Resources\Experiences\Schemas;
 
 use Filament\Schemas\Schema;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TagsInput;
 
 class ExperienceForm
 {
@@ -10,7 +13,12 @@ class ExperienceForm
     {
         return $schema
             ->components([
-                //
+                TextInput::make('title')->required()->label('Cargo'),
+                TextInput::make('company')->required()->label('Empresa'),
+                TextInput::make('start_date')->required()->label('Fecha Inicio'),
+                TextInput::make('end_date')->label('Fecha Fin'),
+                TagsInput::make('technologies')->label('Tecnologías usadas')->placeholder('Escribe una y presiona Enter'),
+                Textarea::make('description')->required()->label('Descripción')->columnSpanFull(),
             ]);
     }
 }
