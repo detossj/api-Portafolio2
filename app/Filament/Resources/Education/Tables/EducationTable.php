979f6d631303ae8ef61e5b3d388f410b38dc2e21
detossj <jorgerubilarssj@gmail.com>
2026-03-14 12:01:27 -0300

    Se agregaron las columnas en EducationTable.php

--- app/Filament/Resources/Education/Tables/EducationTable.php
+++ app/Filament/Resources/Education/Tables/EducationTable.php
@@ -2,10 +2,12 @@
 
 namespace App\Filament\Resources\Education\Tables;
 
+use Filament\Tables\Table;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
-use Filament\Tables\Table;
+use Filament\Actions\DeleteAction;
 
 class EducationTable
 {
@@ -13,13 +15,26 @@ class EducationTable
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('degree')
+                    ->label('Título / Carrera')
+                    ->searchable(),
+                    
+                TextColumn::make('institution')
+                    ->label('Institución'),
+                    
+                TextColumn::make('start_date')
+                    ->label('Año Inicio'),
+                    
+                TextColumn::make('status')
+                    ->label('Estado')
+                    ->badge(), 
             ])
             ->filters([
                 //
             ])
             ->recordActions([
                 EditAction::make(),
+                DeleteAction::make(),
             ])
             ->toolbarActions([
                 BulkActionGroup::make([
