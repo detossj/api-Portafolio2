ec7a8e3d5327aea947a47487de9d1b94b53c22bf
detossj <jorgerubilarssj@gmail.com>
2026-03-14 11:33:46 -0300

    artisan make:filament-resource Education

---
+++ app/Filament/Resources/Education/Schemas/EducationForm.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Filament\Resources\Education\Schemas;
+
+use Filament\Schemas\Schema;
+
+class EducationForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                //
+            ]);
+    }
+}
