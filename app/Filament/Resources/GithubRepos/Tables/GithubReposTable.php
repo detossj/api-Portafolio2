d59ce177f9359bc10b014db137d96735b4e66f8e
detossj <jorgerubilarssj@gmail.com>
2026-03-14 12:13:16 -0300

    Se agregaron las columnas en GithubReposTable.php

--- app/Filament/Resources/GithubRepos/Tables/GithubReposTable.php
+++ app/Filament/Resources/GithubRepos/Tables/GithubReposTable.php
@@ -2,10 +2,12 @@
 
 namespace App\Filament\Resources\GithubRepos\Tables;
 
+use Filament\Tables\Table;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
-use Filament\Tables\Table;
+use Filament\Actions\DeleteAction;
 
 class GithubReposTable
 {
@@ -13,13 +15,31 @@ class GithubReposTable
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('name')
+                    ->label('Nombre')
+                    ->searchable()
+                    ->sortable(),
+
+                TextColumn::make('language')
+                    ->label('Lenguaje')
+                    ->badge()
+                    ->color('info'),
+
+                TextColumn::make('stars')
+                    ->label('Estrellas')
+                    ->icon('heroicon-m-star')
+                    ->sortable(),
+
+                TextColumn::make('url')
+                    ->label('Enlace')
+                    ->limit(30),
             ])
             ->filters([
                 //
             ])
             ->recordActions([
                 EditAction::make(),
+                DeleteAction::make(),
             ])
             ->toolbarActions([
                 BulkActionGroup::make([
