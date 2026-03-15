acf437562dc41bde3029b64404737639236f40ac
detossj <jorgerubilarssj@gmail.com>
2026-03-15 11:04:28 -0300

    Sea agrego que redirija a las tablas en CreateNavItem.php

--- app/Filament/Resources/NavItems/Pages/CreateNavItem.php
+++ app/Filament/Resources/NavItems/Pages/CreateNavItem.php
@@ -8,4 +8,9 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateNavItem extends CreateRecord
 {
     protected static string $resource = NavItemResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
