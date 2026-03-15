7acc55dfd559454ee8e764a1b7613e8ec7216e74
detossj <jorgerubilarssj@gmail.com>
2026-03-15 11:20:52 -0300

    Sea agrego que redirija a las tablas en CreateGithubRepo.php

--- app/Filament/Resources/GithubRepos/Pages/CreateGithubRepo.php
+++ app/Filament/Resources/GithubRepos/Pages/CreateGithubRepo.php
@@ -8,4 +8,9 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateGithubRepo extends CreateRecord
 {
     protected static string $resource = GithubRepoResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
