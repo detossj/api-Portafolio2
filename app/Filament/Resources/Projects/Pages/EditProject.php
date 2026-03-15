a6b67db5b3f8870f9fad5d7dbbf5ae16012b85ea
detossj <jorgerubilarssj@gmail.com>
2026-03-15 11:31:42 -0300

    Sea agrego que redirija a las tablas en EditProject.php

--- app/Filament/Resources/Projects/Pages/EditProject.php
+++ app/Filament/Resources/Projects/Pages/EditProject.php
@@ -16,4 +16,9 @@ class EditProject extends EditRecord
             DeleteAction::make(),
         ];
     }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
