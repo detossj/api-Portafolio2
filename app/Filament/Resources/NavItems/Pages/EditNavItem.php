54b00d45769b26031b939b408203832229ba3f8a
detossj <jorgerubilarssj@gmail.com>
2026-03-15 11:04:37 -0300

    Sea agrego que redirija a las tablas en EditNavItem.php

--- app/Filament/Resources/NavItems/Pages/EditNavItem.php
+++ app/Filament/Resources/NavItems/Pages/EditNavItem.php
@@ -16,4 +16,9 @@ class EditNavItem extends EditRecord
             DeleteAction::make(),
         ];
     }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
