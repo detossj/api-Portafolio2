b4de6a50cc1e3ead0996b8785b8b22ccced474bc
detossj <jorgerubilarssj@gmail.com>
2026-03-15 11:19:43 -0300

    Sea agrego que redirija a las tablas en CreateAboutCard.php

--- app/Filament/Resources/AboutCards/Pages/CreateAboutCard.php
+++ app/Filament/Resources/AboutCards/Pages/CreateAboutCard.php
@@ -8,4 +8,9 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateAboutCard extends CreateRecord
 {
     protected static string $resource = AboutCardResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
