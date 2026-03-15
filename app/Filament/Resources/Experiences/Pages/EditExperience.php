8d0dfb3da7b63420d37dac47d302a6afb8f85952
detossj <jorgerubilarssj@gmail.com>
2026-03-15 11:20:36 -0300

    Sea agrego que redirija a las tablas en EditExperience.php

--- app/Filament/Resources/Experiences/Pages/EditExperience.php
+++ app/Filament/Resources/Experiences/Pages/EditExperience.php
@@ -16,4 +16,9 @@ class EditExperience extends EditRecord
             DeleteAction::make(),
         ];
     }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
