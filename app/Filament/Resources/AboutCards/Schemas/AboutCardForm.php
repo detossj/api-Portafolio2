2d0fd692e0f63bc8f435babd347a5b184b62e7cd
detossj <jorgerubilarssj@gmail.com>
2026-03-15 11:32:13 -0300

    Se agregaron los inputs al formulario en AboutCardForm.php

--- app/Filament/Resources/AboutCards/Schemas/AboutCardForm.php
+++ app/Filament/Resources/AboutCards/Schemas/AboutCardForm.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Resources\AboutCards\Schemas;
 
+use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Schema;
 
 class AboutCardForm
@@ -10,7 +11,10 @@ class AboutCardForm
     {
         return $schema
             ->components([
-                //
+                TextInput::make('icon')->required()->label('Icono (Ej: User, Code)'),
+                TextInput::make('title')->required()->label('Título (Ej: Perfil)'),
+                TextInput::make('text')->required()->label('Texto descriptivo'),
+                TextInput::make('order')->numeric()->default(0)->label('Orden'),
             ]);
     }
 }
