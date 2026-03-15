43f3b53c10c76c5def1742f59c8effbaf6529668
detossj <jorgerubilarssj@gmail.com>
2026-03-15 10:45:13 -0300

    php artisan make:filament-resource NavItem

---
+++ app/Filament/Resources/NavItems/Schemas/NavItemForm.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Filament\Resources\NavItems\Schemas;
+
+use Filament\Schemas\Schema;
+
+class NavItemForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                //
+            ]);
+    }
+}
