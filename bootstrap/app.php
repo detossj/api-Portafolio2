901f9bb51678e41136dbbef9a05e360d1a87e3a7
detossj <jorgerubilarssj@gmail.com>
2026-03-20 14:54:04 -0300

    Fix: Confiar en proxies para arreglar JS de Livewire

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+       // Le dice a Laravel y Livewire que confíen en el proxy de Render
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
