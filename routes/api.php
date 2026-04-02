416a9715fa0e6f8c1280d6efd8777aebf46aa336
detossj <jorgerubilarssj@gmail.com>
2026-04-02 15:44:47 -0300

    Se borraron rutas que ya no se van a usar en api.php

--- routes/api.php
+++ routes/api.php
@@ -4,16 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PortfolioController;
 
 Route::prefix('portfolio')->group(function () {
-    Route::get('/nav', [PortfolioController::class, 'getNav']);
-    Route::get('/hero', [PortfolioController::class, 'getHero']);
-    Route::get('/about', [PortfolioController::class, 'getAbout']);
-    Route::get('/skills', [PortfolioController::class, 'getSkills']);
-    Route::get('/education', [PortfolioController::class, 'getEducation']);
-    Route::get('/experience', [PortfolioController::class, 'getExperience']);
-    Route::get('/projects', [PortfolioController::class, 'getProjects']);
-    Route::get('/contact', [PortfolioController::class, 'getContact']);
-    Route::get('/footer', [PortfolioController::class, 'getFooter']);
-    Route::get('/theme', [PortfolioController::class, 'getTheme']);
     Route::get('/all', [PortfolioController::class, 'getAllData']);
     Route::get('/download-cv', [PortfolioController::class, 'downloadCv']);
 });
