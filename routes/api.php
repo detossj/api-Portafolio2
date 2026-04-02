c46de762c637f0d8395551eb5ed3f705e3bcba15
detossj <jorgerubilarssj@gmail.com>
2026-04-02 15:51:37 -0300

    Se protegieron rutas con middleware en api.php

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PortfolioController;
 
 Route::prefix('portfolio')->group(function () {
-    Route::get('/all', [PortfolioController::class, 'getAllData']);
-    Route::get('/download-cv', [PortfolioController::class, 'downloadCv']);
+    
+    Route::middleware(['api.key'])->group(function () { 
+        Route::get('/all', [PortfolioController::class, 'getAllData']);
+        Route::get('/download-cv', [PortfolioController::class, 'downloadCv']);
+    });
+   
 });
