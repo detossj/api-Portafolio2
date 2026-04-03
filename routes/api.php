ac861bf98a1dd75da46835350bc19cf247589cf1
deto <jorgerubilarssj@gmail.com>
2026-04-03 18:09:14 -0300

    Se agrego otro middleware en las rutas de api.php

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PortfolioController;
 
 Route::prefix('portfolio')->group(function () {
-    
-    Route::middleware(['api.key'])->group(function () { 
+    // En este grupo de rutas, aplicamos el middleware 'api.key' para validar la clave de API
+    // y el middleware 'throttle:portfolio_api' para limitar las peticiones a 60 por minuto por
+    // IP que se definió en AppServiceProvider.
+    Route::middleware(['api.key', 'throttle:portfolio_api'])->group(function () { 
         Route::get('/all', [PortfolioController::class, 'getAllData']);
         Route::get('/download-cv', [PortfolioController::class, 'downloadCv']);
     });
